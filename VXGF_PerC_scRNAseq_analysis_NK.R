fa6de69bd2b7cbd4bb015d46970b41d37b9d4bd6
Niklas Krupka <niklas.krupka@gmail.com>
2018-08-26 10:01:27 -0400

    Just comments

--- VXGF_PerC_scRNAseq_analysis_NK.R
+++ VXGF_PerC_scRNAseq_analysis_NK.R
@@ -1,11 +1,12 @@
-##### Ploting and analysis script for scRNAseq data for IgA paper
-##### This analysis script expects a Seurat object that was previously created
+##### Ploting and analysis script for scRNAseq data for IgA manuscript
+##### This analysis script expects a previously created Seurat object and
+##### a pre-generated list of marker genes per cluster.
 ##### Niklas Krupka, 2018-08-16
 
 library(Seurat)
 library(tidyverse)
-library(stringr)   # To extract the WT vs KO identifiers in a simple way
-library(cowplot)   # For the combined violin plot
+library(stringr)   # To extract the WT vs KO identifiers
+library(cowplot)   # For the combined violin plots
 
 ##### Define parameters for tSNE plotting
 NK_TSNEPlot <- function(seuratobject, ...){
@@ -15,27 +16,27 @@ NK_TSNEPlot <- function(seuratobject, ...){
 
 ##### Define a gene list that are interesting and should be shown in the
 ##### various plots
-genelist <- c("Csf1r",  # Best Pan-macrophage marker in the cavities
-              "Adgre1", # F4/80. high in LPM
-              "Itgam",  # Cd11b, high in LPM
-              "Icam2",  # CD102, high in LPM
-              "Timd4",  # Some LPM express Tim4
-              "H2-Aa",  # MHCII, high in SPM
-              "Retnla", # RELMalpha, high in SPM
-              "Ccr2",   # CCR2
-              "Cd209a", # DC-SIGN  
-              "Mki67", "Top2a", # Prolif
-              "Map1lc3a", # Interesting gene: LC3+ Macs
-              "Folr2",   # Interesting gene: Folate receptor+ Macs
-              "Notch2", # Interesting gene
-              "Ms4a1", "Cd79a", # BC markers
-              "Fcer2a", "Fcmr", "Lifr", # Receptors and BC markers
+genelist <- c("Csf1r",  
+              "Adgre1", # F4/80
+              "Itgam",  # Cd11b
+              "Icam2",  # CD102
+              "Timd4",  
+              "H2-Aa",  
+              "Retnla", # RELMalpha
+              "Ccr2",   
+              "Cd209a", 
+              "Mki67", "Top2a", 
+              "Map1lc3a",
+              "Folr2",  
+              "Notch2", 
+              "Ms4a1", "Cd79a",
+              "Fcer2a", "Fcmr", "Lifr",
               "Cd3e", "Cd4", "Nkg7")
 
-##### Load Seurat object and markers (obtained by FindAllMarkers with
-##### default arguments). Then calculate the Top10 markers per cluster
-load(file = "xbp1.tSNE.RData")
-xbp1.markers <- readRDS("./xbp1markers.rds")
+##### Load Seurat object and markers
+##### Then calculate the Top10 markers per cluster
+load(file = "./xbp1.tSNE.RData")
+xbp1.markers <- readRDS("./xbp1markers.rds") ### obtained by FindAllMarkers with default arguments).
 xbp1.top10markers <- xbp1.markers %>% 
   group_by(cluster) %>% 
   top_n(10, avg_logFC)
@@ -67,7 +68,7 @@ NK_TSNEPlot(xbp1)
 xbp1 <- SetAllIdent(xbp1, id = "res.1")
 
 ##### Violin plot of the indicated genes
-##### This first creates a list of plots using Seurat, 
+##### This first creates a list of plots using the Seurat function
 ##### which is then modified by lapply and 
 ##### plotted in a nice grid using cowplot
 plot.vln <- VlnPlot(object = xbp1, features.plot = genelist, 
